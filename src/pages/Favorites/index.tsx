cd3608b501b955f49f47204bc637a82eb1dda66d
Geovani José <geovani-lol@hotmail.com>
2021-01-06 21:08:44 -0300

    Implement Favorites page

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,352 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Card,
+  CardContent,
+  Checkbox,
+  CircularProgress,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
+import {
+  FilterListRounded,
+  ClearRounded,
+  DoneRounded,
+  Star,
+} from '@material-ui/icons';
+import { Skeleton } from '@material-ui/lab';
+
+import { useFavorite } from '../../hooks/favorite';
+
+interface CheckboxOptionsData {
+  nerdy: boolean;
+  explicit: boolean;
+}
+
+interface JokeItem {
+  id: number;
+  joke: string;
+  categories: string[];
+  favorite: boolean;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(0, 9),
+    },
+    filterContainer: {
+      maxWidth: 730,
+      height: 160,
+      margin: '0 auto',
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    filter: {
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      borderBottomLeftRadius: 20,
+      background: theme.palette.primary.main,
+    },
+    formControl: {
+      marginTop: theme.spacing(2),
+      color: theme.palette.background.default,
+      '& > .MuiFormLabel-root': {
+        color: theme.palette.background.default,
+        '&.Mui-focused': {
+          color: theme.palette.background.default,
+        },
+      },
+    },
+    checkbox: {
+      color: theme.palette.background.default,
+    },
+    iconsContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      padding: theme.spacing(1),
+      borderBottomRightRadius: 20,
+      background: theme.palette.primary.main,
+    },
+    icon: {
+      fontSize: 30,
+      color: theme.palette.background.default,
+    },
+    iconCheck: {
+      fontSize: 30,
+      color: theme.palette.success.main,
+    },
+    iconCancel: {
+      fontSize: 30,
+      color: theme.palette.error.main,
+    },
+    progressBar: {
+      color: theme.palette.success.main,
+      marginTop: 15,
+      marginBottom: 15,
+    },
+    mainContainer: {
+      padding: theme.spacing(4, 0),
+    },
+    jokesGridTitle: {
+      color: theme.palette.secondary.dark,
+    },
+    jokesGrid: {
+      listStyle: 'none',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(350px, 1fr))',
+      gridGap: theme.spacing(3),
+      margin: theme.spacing(4, 0),
+      padding: 0,
+    },
+    jokesCard: {
+      position: 'relative',
+      height: '100%',
+      display: 'flex',
+      background: theme.palette.secondary.dark,
+      color: theme.palette.primary.dark,
+    },
+    jokesCardContent: {
+      flex: 1,
+      textAlign: 'justify',
+      padding: theme.spacing(3),
+    },
+    jokesCardIcons: {
+      position: 'absolute',
+      width: 30,
+      top: 0,
+      right: 0,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderBottomLeftRadius: 15,
+      background: theme.palette.primary.main,
+    },
+    iconStarBorder: {
+      color: theme.palette.secondary.dark,
+    },
+    iconStarFilled: {
+      color: theme.palette.warning.main,
+    },
+  }),
+);
+
+const Favorites: React.FC = () => {
+  const classes = useStyles();
+
+  const { favoriteJokes, removeFavoriteJoke } = useFavorite();
+
+  const [jokes, setJokes] = useState<JokeItem[]>([]);
+  const [showFilter, setShowFilter] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [checkboxOptions, setCheckboxOptions] = useState<CheckboxOptionsData>({
+    nerdy: false,
+    explicit: false,
+  });
+
+  useEffect(() => {
+    setJokes(favoriteJokes);
+  }, [favoriteJokes]);
+
+  const handleShowFilter = useCallback(() => {
+    setShowFilter(!showFilter);
+  }, [showFilter]);
+
+  const handleCheckboxChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCheckboxOptions({
+        ...checkboxOptions,
+        [event.target.name]: event.target.checked,
+      });
+    },
+    [checkboxOptions],
+  );
+
+  const handleJokeFavorite = useCallback(
+    (id: number) => {
+      removeFavoriteJoke(id);
+    },
+    [removeFavoriteJoke],
+  );
+
+  const handleFilterDone = useCallback(() => {
+    if (!checkboxOptions.nerdy && !checkboxOptions.explicit) {
+      setJokes(favoriteJokes);
+      return;
+    }
+
+    setIsLoading(true);
+
+    let filteredJokes = [] as JokeItem[];
+
+    if (checkboxOptions.nerdy) {
+      filteredJokes = favoriteJokes.filter(joke =>
+        joke.categories.includes('nerdy'),
+      );
+    }
+
+    if (checkboxOptions.explicit) {
+      filteredJokes = [
+        ...filteredJokes,
+        ...favoriteJokes.filter(joke => joke.categories.includes('explicit')),
+      ];
+    }
+
+    setJokes(filteredJokes);
+    setIsLoading(false);
+  }, [favoriteJokes, checkboxOptions]);
+
+  return (
+    <Container className={classes.root}>
+      <Box className={classes.filterContainer}>
+        {showFilter && (
+          <Container className={classes.filter}>
+            <FormControl component="fieldset" className={classes.formControl}>
+              <FormLabel component="legend">Categories</FormLabel>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    // eslint-disable-next-line react/jsx-wrap-multilines
+                    <Checkbox
+                      className={classes.checkbox}
+                      name="nerdy"
+                      checked={checkboxOptions.nerdy}
+                      onChange={handleCheckboxChange}
+                    />
+                  }
+                  label="Nerdy"
+                />
+                <FormControlLabel
+                  control={
+                    // eslint-disable-next-line react/jsx-wrap-multilines
+                    <Checkbox
+                      className={classes.checkbox}
+                      name="explicit"
+                      checked={checkboxOptions.explicit}
+                      onChange={handleCheckboxChange}
+                    />
+                  }
+                  label="Explicit"
+                />
+              </FormGroup>
+            </FormControl>
+          </Container>
+        )}
+
+        <Box className={classes.iconsContainer}>
+          {!showFilter ? (
+            <IconButton
+              className={classes.icon}
+              component="span"
+              onClick={handleShowFilter}
+              title="Show filter"
+            >
+              <FilterListRounded fontSize="inherit" />
+            </IconButton>
+          ) : (
+            <>
+              <IconButton
+                className={classes.iconCancel}
+                component="span"
+                onClick={handleShowFilter}
+                title="Cancel"
+              >
+                <ClearRounded fontSize="inherit" />
+              </IconButton>
+              {!isLoading ? (
+                <IconButton
+                  className={classes.iconCheck}
+                  component="span"
+                  onClick={handleFilterDone}
+                  title="Done"
+                >
+                  <DoneRounded fontSize="inherit" />
+                </IconButton>
+              ) : (
+                <CircularProgress
+                  className={classes.progressBar}
+                  size={24}
+                  color="secondary"
+                />
+              )}
+            </>
+          )}
+        </Box>
+      </Box>
+
+      <Box className={classes.mainContainer}>
+        <Typography
+          component="h1"
+          variant="h4"
+          className={classes.jokesGridTitle}
+        >
+          Favorites
+        </Typography>
+        <ul className={classes.jokesGrid}>
+          {!isLoading &&
+            jokes.map(joke => (
+              <li key={joke.id}>
+                <Card elevation={3} className={classes.jokesCard}>
+                  <CardContent className={classes.jokesCardContent}>
+                    <Typography component="strong" variant="h6">
+                      {joke.joke}
+                    </Typography>
+                  </CardContent>
+                  <section className={classes.jokesCardIcons}>
+                    <IconButton
+                      component="span"
+                      size="small"
+                      onClick={() => handleJokeFavorite(joke.id)}
+                      title="Favorite"
+                    >
+                      {joke.favorite ? (
+                        <Star
+                          className={classes.iconStarFilled}
+                          fontSize="default"
+                        />
+                      ) : (
+                        <Star
+                          className={classes.iconStarBorder}
+                          fontSize="default"
+                        />
+                      )}
+                    </IconButton>
+                  </section>
+                </Card>
+              </li>
+            ))}
+
+          {isLoading &&
+            [0, 1, 2, 3].map(numb => (
+              <li key={numb}>
+                <Card elevation={3} className={classes.jokesCard}>
+                  <CardContent className={classes.jokesCardContent}>
+                    <Typography variant="h6">
+                      <Skeleton width="100%" animation="wave" />
+                      <Skeleton width="100%" animation="wave" />
+                      <Skeleton width="40%" animation="wave" />
+                    </Typography>
+                  </CardContent>
+                  <section className={classes.jokesCardIcons}>
+                    <IconButton component="span" size="small">
+                      <Skeleton width={15} animation="wave" />
+                    </IconButton>
+                  </section>
+                </Card>
+              </li>
+            ))}
+        </ul>
+      </Box>
+    </Container>
+  );
+};
+
+export default Favorites;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Random from '../pages/Random';
+import Favorites from '../pages/Favorites';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
     <Route path="/random" component={Random} />
+    <Route path="/favorites" component={Favorites} />
   </Switch>
 );
 
